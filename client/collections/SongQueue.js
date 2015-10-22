c3c764c9bdd4210d958a8899c03099add0b5d168
apiga <ambroise.piganeau@gmail.com>
2015-10-21 17:00:31 -0700

    implemented a dequeue when an element is clicked on in thesongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,6 +1,14 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
   initialize: function() {
+    // this.on('enqueue', function() {
+    this.on('dequeue', function(song) {
+      this.remove(song);
+    });
+    this.on('ended', function() {
+      this.shift();
+      this.models[0].play();
+    });
   },
   enqueue: function(song) {
     this.push(song);
